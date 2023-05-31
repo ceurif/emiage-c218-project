378a17c677858f1200776198166b5c7a517c958c
Cherif DIOUF <diouf-cherif@live.fr>
2023-05-31 15:25:22 +0000

    Refact on responsivity

--- includes/nav.php
+++ includes/nav.php
@@ -3,9 +3,11 @@
         <div class="nav-wrapper nav-wrapper-2 container white">
             <ul class="left">
                 <li><a href="index" class="brand"></a></li>
+                <li><a href="sign.php" class="log-in hide-on-large-only">Sign In |</a></li>
+                <li><a href="sign-up.php" class="log-in hide-on-large-only">Sign Up |</a></li>
             </ul>
-            <ul class="right">
-                <li class="hide-on-med-and-down"><a href="index" class="dark-text">Home</a></li>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="index" class="dark-text">Home</a></li>
                 <li><a href="sign.php" class="waves-effect waves-light btn button-rounded">Sign In</a></li>
                 <li><a href="sign-up.php" class="waves-effect waves-light btn btn-secondary button-rounded">Sign Up</a></li>
                 <li class="hide-on-med-and-down"><a href="cart" class="dark-text baskett"><i class="material-icons">shopping_cart</i>
